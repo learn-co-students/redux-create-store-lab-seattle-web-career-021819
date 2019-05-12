e159fb20435179e9cd33a929957c48cead035c04
ytsuji27 <ytsuji91@gmail.com>
2019-05-11 17:13:45 -0700

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,6 +1,22 @@
 export default function createStore(reducer) {
-  // add your code here
-}
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  };
+
+  dispatch({ type: '@@Init' });
+  function getState() {
+    return state;
+  };
+
+  return {
+    dispatch,
+    getState
+  };
+
+};
 
 function render() {
   const container = document.getElementById('container');
